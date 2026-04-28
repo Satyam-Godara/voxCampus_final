98838ec232ee5f59fd3bdeba40e81092c087f376
Satyam Godara <satyamgodara2005@gmail.com>
2026-04-28 13:50:19 +0530

    updated the backend api url in frontend api file

--- client/voxcampus-frontend/src/utils/api.js
+++ client/voxcampus-frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://voxcampus-final.onrender.com/api' });
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('vox_token');
