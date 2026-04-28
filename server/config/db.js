5ae63690c759a1dca3cd3ce24e2bfe8e0586d239
Satyam Godara <satyamgodara2005@gmail.com>
2026-04-28 12:40:03 +0530

    mongo url updated

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // const conn = await mongoose.connect(process.env.MONGODB_URI);
-    const conn = await mongoose.connect("mongodb://localhost:27017/voxcampus");
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    // const conn = await mongoose.connect("mongodb://localhost:27017/voxcampus");
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
